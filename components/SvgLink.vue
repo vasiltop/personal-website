fc3f09f2c868c604b80bea246eceadf82113505b
Vasil <vasiltopalovic@outlook.com>
2023-10-19 00:10:31 -0400

    feat: scroll animations

---
+++ components/SvgLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ link: string }>();
+</script>
+
+<template>
+	<NuxtLink
+		:to="link"
+		target="_blank"
+		class="hover:scale-110 duration-300 transition-all"
+	>
+		<slot />
+	</NuxtLink>
+</template>
